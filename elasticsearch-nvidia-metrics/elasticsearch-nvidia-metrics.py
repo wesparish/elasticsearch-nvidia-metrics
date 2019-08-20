31f21ef8afbb640c4e5ef64b1f1bf19cad87ca3d
Wes Parish <wes@elastiscale.net>
2019-08-20 12:43:42 +0000

    Fixing timestamp in ES doc

--- elasticsearch-nvidia-metrics/elasticsearch-nvidia-metrics.py
+++ elasticsearch-nvidia-metrics/elasticsearch-nvidia-metrics.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 
-# root@claymore-nvidia-scalable-84b5d66556-gx5cg:/home/claymore# nvidia-smi --query-gpu=index,gpu_uuid,fan.speed,temperature.gpu,memory.used --format=csv,noheader,nounits | tr -d ' '
-# 0,GPU-9c9290a3-b54e-adb6-02ff-9cf106b50ca3,47,65,3403
-# 1,GPU-adf23210-605a-ade1-549d-9fc5996f1f9c,45,64,3403
-# 2,GPU-72ddad7f-26cb-9d2b-8a74-781877b7d575,49,68,3403
-# 3,GPU-cd6b309d-8885-e7d2-6aae-834381efa24a,46,65,3403
-# 4,GPU-29b23a6a-d622-9f37-4e18-9ed0cfde2daa,49,68,3403
-# 5,GPU-ef77ef4a-2670-76ea-4be0-92c90c173a41,51,69,3403
-
 from elasticsearch import Elasticsearch
 import random, time, os
 from functools import reduce  # forward compatibility for Python 3
 import operator
 import subprocess
 import csv,json
-from io import StringIO
+import StringIO
 import datetime
+import signal
+import datetime
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+signal.signal(signal.SIGINT, signal_handler)
+interrupted = False
 
 es = Elasticsearch(os.getenv('ES_HOST_LIST', 'http://elasticsearch.jamiehouse:9200').split(','), 
                    maxsize=os.getenv('ES_MAX_CONNECTIONS', 25))
 
-# >>> retval = subprocess.check_output(["nvidia-smi", "--query-gpu=index,gpu_uuid,fan.speed,temperature.gpu,memory.used", "--format=csv,noheader,nounits"])
-#>>> retval
-#'0, GPU-9c9290a3-b54e-adb6-02ff-9cf106b50ca3, 44, 65, 3403\n1, GPU-adf23210-605a-ade1-549d-9fc5996f1f9c, 43, 64, 3403\n2, GPU-72ddad7f-26cb-9d2b-8a74-781877b7d575, 47, 68, 3403\n3, GPU-cd6b309d-8885-e7d2-6aae-834381efa24a, 44, 65, 3403\n4, GPU-29b23a6a-d622-9f37-4e18-9ed0cfde2daa, 47, 68, 3403\n5, GPU-ef77ef4a-2670-76ea-4be0-92c90c173a41, 48, 69, 3403\n'
 def get_nvidia_metrics():
   if os.getenv('MOCK', False):
     metrics_str = '0, GPU-9c9290a3-b54e-adb6-02ff-9cf106b50ca3, 44, 65, 3403\n1, GPU-adf23210-605a-ade1-549d-9fc5996f1f9c, 43, 64, 3403\n2, GPU-72ddad7f-26cb-9d2b-8a74-781877b7d575, 47, 68, 3403\n3, GPU-cd6b309d-8885-e7d2-6aae-834381efa24a, 44, 65, 3403\n4, GPU-29b23a6a-d622-9f37-4e18-9ed0cfde2daa, 47, 68, 3403\n5, GPU-ef77ef4a-2670-76ea-4be0-92c90c173a41, 48, 69, 3403\n'
@@ -31,24 +29,32 @@ def get_nvidia_metrics():
   return metrics_str
 
 def send_to_elasticsearch(metrics_str):
-  f = StringIO(metrics_str)
+  f = StringIO.StringIO(metrics_str)
   reader = csv.DictReader(f, delimiter=',', fieldnames=['index', 'gpu_uuid', 'fanspeed', 'temperaturegpu', 'memoryused'], skipinitialspace=True)
   for row in reader:
-    json_output = json.dumps(row)
-    # Add additional data to json
-    json_data = json.loads(json_output)
-    json_data['node_name'] = os.getenv('NODE_NAME', os.getenv('HOSTNAME', 'localhost'))
-    json_output = json.dumps(json_data)
+    doc = {
+      'timestamp': datetime.datetime.now(),
+      'index': int(row['index']),
+      'gpu_uuid': row['gpu_uuid'],
+      'fanspeed': int(row['fanspeed']),
+      'temperaturegpu': int(row['temperaturegpu']),
+      'memoryused': int(row['memoryused']),
+      'node_name': os.getenv('NODE_NAME', os.getenv('HOSTNAME', 'localhost'))
+    }
 
-    print("json: %s" % json_output)
+    if os.getenv("DEBUG", False):
+      print("doc: %s" % doc)
     retval = es.index(index=os.getenv('ES_INDEX', 'nvidia-metrics') + '-' + datetime.datetime.now().strftime('%Y.%m.%d'),
                       doc_type='nvidia-metrics',
-                      body=json_output)
+                      body=doc)
 
 if __name__ == '__main__':
   # Generate some requests.
-  while True:
-    metrics_str = get_nvidia_metrics()
-    send_to_elasticsearch(metrics_str)
+  while not interrupted:
+    try:
+      metrics_str = get_nvidia_metrics()
+      send_to_elasticsearch(metrics_str)
+    except ex:
+      print("Caught exception: " + ex)
     time.sleep(os.getenv('SLEEP_TIME', 10))
 
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ NAME = 'elasticsearch-nvidia-metrics'
 
 setup(
     name=NAME,
-    version='1.0.0',
+    version='1.0.7',
     packages=[NAME,],
     license='apache2',
     long_description=open('README').read(),
     install_requires=[
-        'elasticsearch>=7.0.0,<8.0.0'
+        'elasticsearch>=7.0.0,<8.0.0',
     ],
     url='https://github.com/wesparish/' + NAME,
     author='Wes Parish',
